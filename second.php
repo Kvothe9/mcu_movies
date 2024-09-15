f414b0fa9b57e9d4adede832bcb081615048531d
Kvothe9 <rprobertopelayo@gmail.com>
2024-09-15 02:45:52 +0200

    second.php

--- second.php
+++ second.php
@@ -38,7 +38,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Próxima Película de Marvel</title>
+    <title>Próximo estreno de Marvel</title>
     <link
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
